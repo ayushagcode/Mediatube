f4e8ef8b35036ece29f0fdaab38b41269580dc03
Ayush Agarwal <114360050+ayushagcode@users.noreply.github.com>
2025-01-19 21:47:11 +0530

    utils code

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@ import cors from "cors";
 
 const app = express();
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
+    origin: process.env.CORS_ORIGIN,// to whom origin allowed
+    credentifals: true,
 }));
 
 app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true, limit: "16kb"}));
+app.use(express.urlencoded({extended:true, limit: "16kb"}));// to decode the url
 app.use(express.static("public"));
 app.use(cookieParser());
 
