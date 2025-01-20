78491ad9800ed2b122999b23ae7b6dd11d955fd1
Ayush Agarwal <114360050+ayushagcode@users.noreply.github.com>
2025-01-20 16:31:08 +0530

    upload file

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    // file -> multer handle karne ke liye use
+    destination: function (req, file, cb) {
+      cb(null, './public/temp')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    //   read doc
+    }
+  })
+  
+export const upload = multer({
+    storage: storage,
+})
